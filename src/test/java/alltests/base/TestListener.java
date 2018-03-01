9701b4b98cefeaf303ba8885f58595a916d78c95
shuvashishny <shuvashishny@gmail.com>
2018-03-01 13:22:53 -0500

    Added testng listener file

--- src/test/java/alltests/base/TestListener.java
+++ src/test/java/alltests/base/TestListener.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 /**
- * Created by Shuvashish on 2/22/2018.
+ * Created by Shuvashish on 2/28/2018.
  */
 public class TestListener extends TestListenerAdapter {
 
@@ -20,7 +21,7 @@ public class TestListener extends TestListenerAdapter {
                 .getScreenshotAs(OutputType.FILE);
 
         try {
-            Files.copy(fileSource.toPath(), new File(TestBase.getSrcFolder()+tr.getName()+".png").toPath());
+            Files.copy(fileSource.toPath(), new File(TestBase.getSrcFolder() + tr.getName() + ".png").toPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
